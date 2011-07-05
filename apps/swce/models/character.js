e9808695ad260621174ab7d4eb7407e295d3043d
Benjamin Horsleben <benjamin@fizkerinc.dk>
2011-07-06 00:21:15 +0200

    Added some derived stats.

--- apps/swce/models/character.js
+++ apps/swce/models/character.js
@@ -22,4 +22,13 @@ SWCE.Character = SC.Record.extend(
 	attr: SC.Record.toMany('SWCE.CharAttribute', { inverse: 'char', isMaster: YES }),
 	skills: SC.Record.toMany('SWCE.CharSkill', { inverse: 'char', isMaster: YES }),
 	edges: SC.Record.toMany('SWCE.CharEdge', { inverse: 'char', isMaster: YES }),
+
+	charisma: SC.Record.attr(Number, { defaultValue: 0 }),
+	pace_die: SC.Record.attr(String, { defaultValue: 'd6' }),
+	pace_val: SC.Record.attr(Number, { defaultValue: 6 }),
+	
+	pace: function() {
+		// TODO: Add edges, gear or encumbrance to this
+		return this.get('pace_val') + ' (' + this.get('pace_die') + ')';
+	}.property('pace_die', 'pace_val').cacheable()
 }) ;
